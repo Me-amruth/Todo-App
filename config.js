489f8e46faa4a9e389e5473610358b90aca1ef1b
Amruth <amruthhh@outlook.com>
2024-09-01 17:23:02 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  PORT: process.env.PORT || 3000,
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://amruth:A1M2R3U4T5H@amruth.wnylfrc.mongodb.net/?retryWrites=true&w=majority&appName=Amruth'
+}
